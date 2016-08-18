21632eb95fe86b9e849006703512ec5292516be9
gailtown <at0451@bard.edu>
2016-08-18 12:18:47 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "How are you? Can I help you with anything?"
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
